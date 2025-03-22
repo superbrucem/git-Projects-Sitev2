958247868f7049f4fe4af0f80bcffbfa7fbce417
superbrucem <superbrucem@gmail.com>
2025-03-22 17:03:50 -0400

    working8

--- server/vite.ts
+++ server/vite.ts
@@ -72,6 +72,7 @@ export async function setupVite(app: Express, server: Server) {
 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
+  const publicPath = path.resolve(__dirname, "..", "public");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
@@ -80,6 +81,7 @@ export function serveStatic(app: Express) {
   }
 
   app.use(express.static(distPath));
+  app.use(express.static(publicPath));
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
--- vite.config.ts
+++ vite.config.ts
@@ -29,8 +29,10 @@ export default defineConfig({
     },
   },
   root: path.resolve(__dirname, "client"),
+  publicDir: path.resolve(__dirname, "public"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
+    copyPublicDir: true,
   },
 });
