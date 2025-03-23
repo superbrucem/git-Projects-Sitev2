daaedb107879b8cad3331cf99a44aa58988a3f6c
superbrucem <superbrucem@gmail.com>
2025-03-22 20:01:09 -0400

    startover1

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,8 +1,5 @@
-import Image from 'next/image'
-
-<Image 
-  src="/images/bruce-profile.png"
+<img 
+  src="/images/bruce-profile.png" 
   alt="Bruce profile"
-  width={500}  // adjust as needed
-  height={500} // adjust as needed
 />
+
